baa976e766cf8b237f5cf88b111d2130d06d73df
minseoky <minseok184@gmail.com>
2023-04-11 10:12:42 +0900

    [#02] 기본 레이아웃 작성, 간단한 css

--- src/App.js
+++ src/App.js
@@ -1,12 +1,19 @@
 import Header from "./components/Header.js"
 import Footer from "./components/Footer.js"
 
+
 function App() {
   return (
     <div className="App">
         <Header/>
-            <h3>Title</h3>
-            <p>paragraph</p>
+            <h3>자기소개 페이지</h3>
+            <p>저는 끊임없이 도전하고 성장하는 학생 개발자 최민석입니다. </p>
+            <p>새로운 언어와 기술을 배우는 것을 즐기며, 항상 최신 개발 동향과 트렌드를 익히려 노력합니다.</p>
+            <p>높은 호기심과 열정을 가지고 다양한 프로젝트에 도전하며 지속적인 개발자로 성장하고 있습니다.</p>
+
+
+
+
         <Footer/>
     </div>
   );
--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,9 +1,18 @@
 import styles from "../css/Footer.module.css"
+import {FaEnvelope, FaGithub} from "react-icons/fa";
 function Footer(){
 
     return(
         <div className={styles.container}>
             Hi, i'm Footer
+            <ul className={styles.lists}>
+                <li>
+                    <a href="https://github.com/minseoky/Mid-Term-Project"><FaGithub />Github</a>
+                </li>
+                <li>
+                    <a href="mailto:minseok184@gmail.com"><FaEnvelope />Gmail</a>
+                </li>
+            </ul>
         </div>
     )
 }
