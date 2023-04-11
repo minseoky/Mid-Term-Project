c81ebca4920f3d562b70c08103e06ce3dfb64349
minseoky <minseok184@gmail.com>
2023-04-11 11:22:47 +0900

    [#06] Nav Footer 레이아웃 수정, css 추가

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,20 +1,24 @@
-import styles from "../css/Footer.module.css"
-import {FaEnvelope, FaGithub} from "react-icons/fa";
-function Footer(){
+import styles from "../css/Footer.module.css";
+import { FaEnvelope, FaGithub } from "react-icons/fa";
 
-    return(
+function Footer() {
+    return (
         <div className={styles.container}>
-            Hi, i'm Footer
-            <ul className={styles.lists}>
-                <li>
-                    <a href="https://github.com/minseoky/Mid-Term-Project"><FaGithub />Github</a>
-                </li>
-                <li>
-                    <a href="mailto:minseok184@gmail.com"><FaEnvelope />Gmail</a>
-                </li>
-            </ul>
+            <div className={styles.lists}>
+                <a href="https://github.com/minseoky/Mid-Term-Project" >
+                    <FaGithub />
+                    <span>Github</span>
+                </a>
+                <a href="mailto:minseok184@gmail.com">
+                    <FaEnvelope />
+                    <span>Gmail</span>
+                </a>
+            </div>
+            <div className={styles.text}>
+                copyright &copy; {new Date().getFullYear()} All rights reserved by <a href="https://github.com/minseoky" className={styles.link}>Minseoky</a>
+            </div>
         </div>
-    )
+    );
 }
 
 export default Footer;
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,11 +1,24 @@
-import styles from "../css/Header.module.css"
-function Header(){
+// Header.js
 
-    return(
+import React from "react";
+import styles from "../css/Header.module.css"; // Header 컴포넌트의 CSS 파일 import
+
+function Header() {
+    return (
         <div className={styles.container}>
-            Hi, i'm header
+            <h1 className={styles.title}>My Portfolio</h1>
+            <nav className={styles.navbar}>
+                <ul className={styles.navlist}>
+
+                    <li className={styles.navitem}>
+                        <a href="http://www.minseoky.me" rel="noreferrer" target="_blank" className={styles.navlink}>
+                            Blog
+                        </a>
+                    </li>
+                </ul>
+            </nav>
         </div>
-    )
+    );
 }
 
 export default Header;
